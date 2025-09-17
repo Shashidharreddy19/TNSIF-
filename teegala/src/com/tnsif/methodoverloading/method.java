d2055f7ae0b4de03f5fb9590aed3e2221125153c
shash <shash@192.168.1.57>
2025-09-17 22:41:56 +0530

    17-09-2025

---
+++ teegala/src/com/tnsif/methodoverloading/method.java
@@ -0,0 +1,42 @@
+package com.tnsif.methodoverloading;
+
+public class method {
+
+		    // Method 1: Add two integers
+		    public int add(int a, int b) {
+		        return a + b;
+		    }
+
+		    // Method 2: Add two double numbers (different data type)
+		    public double add(double a, double b) {
+		        return a + b;
+		    }
+
+		    // Method 3: Add three integers (different number of parameters)
+		    public int add(int a, int b, int c) {
+		        return a + b + c;
+		    }
+
+		    // Method 4: Add int and double (different sequence of parameters)
+		    public double add(int a, double b) {
+		        return a + b;
+		    }
+
+		    // Method 5: Add double and int (different sequence of parameters)
+		    public double add(double a, int b) {
+		        return a + b;
+		    }
+
+		    public static void main(String[] args) {
+		        method obj = new method();
+
+		        System.out.println("Sum of 2 integers: " + obj.add(5, 10));              // Method 1
+		        System.out.println("Sum of 2 doubles: " + obj.add(5.5, 10.5));        // Method 2
+		        System.out.println("Sum of 3 integers: " + obj.add(1, 2, 3));         // Method 3
+		        System.out.println("Sum of int and double: " + obj.add(5, 10.5));    // Method 4
+		        System.out.println("Sum of double and int: " + obj.add(5.5, 10));    // Method 5
+		    }
+		}
+
+
+
---
+++ teegala/src/constructor/constructoroverloading.java
@@ -0,0 +1,53 @@
+package constructor;
+
+public class constructoroverloading {
+
+	public static void main(String[] args) {
+		class Student {
+		    private int id;
+		    private String name;
+		    private double marks;
+
+		    // Constructor 1: No parameters
+		    public Student() {
+		        id = 0;
+		        name = "Unknown";
+		        marks = 0.0;
+		    }
+
+		    // Constructor 2: Two parameters
+		    public Student(int id, String name) {
+		        this.id = id;
+		        this.name = name;
+		        marks = 0.0;
+		    }
+
+		    // Constructor 3: Three parameters
+		    public Student(int id, String name, double marks) {
+		        this.id = id;
+		        this.name = name;
+		        this.marks = marks;
+		    }
+
+		    // Method to display student details
+		    public void display() {
+		        System.out.println("Student ID: " + id);
+		        System.out.println("Student Name: " + name);
+		        System.out.println("Marks: " + marks);
+		        System.out.println("----------------------");
+		    }
+		}
+
+		        // Using Constructor 1
+		        Student s1 = new Student();
+		        s1.display();
+
+		        // Using Constructor 2
+		        Student s2 = new Student(101, "Alice");
+		        s2.display();
+
+		        // Using Constructor 3
+		        Student s3 = new Student(102, "Bob", 85.5);
+		        s3.display();
+		    }
+		}
